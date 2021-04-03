6660fe9f49598475346b728529b48dbbbe813bb2
thomasschmitz <thomas.schmitz85@icloud.com>
2021-04-03 22:10:08 +0200

    python module with data sets and rest api

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Restaurant(models.Model):
+    key = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+class Dish(models.Model):
+    ticker  = models.CharField(max_length=200)
+    exchange = models.CharField(max_length=200)
+    signal = models.CharField(max_length=200)
+    volume = models.FloatField()
+    volumeindikator = models.CharField(max_length=200)
+    datetime = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    orgm = models.CharField(max_length=200)
+    status = models.CharField(max_length=200,default="open")
+    previoustrade = models.IntegerField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.RESTRICT)
+
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,23 @@ from django.urls import path
 
 from . import views
 
+#TODO: JSON URL interpretieren als Test
+# curl -H 'Content-Type: application/json; charset=utf-8' -d '{"text": "BTCUSD Greater Than 9000"}' -X POST https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+
+#Todo: weitere befehle aus Tradingview interpretieren
+
+#todo: verbindung mit einer auth Kennzeichnung absichern
+
+#todo: nur gewisse IP adressen erlauben
+
+#todo nur https erlauben
+
+#todo Request in eine Tabelle schreiben time, request and return, dauer
+
+#todo: daraus resultierenden Profit in die Tabelle schreiben bei close
+
+
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
